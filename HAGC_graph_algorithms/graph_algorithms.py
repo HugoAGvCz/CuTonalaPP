8c85491992e0c2d798e440081f6ea99df90046b0
Hugo Galván <118332303+HugoAGvCz@users.noreply.github.com>
2024-09-17 23:29:51 -0600

    Add commented code in graph_algorithms.py

--- HAGC_graph_algorithms/graph_algorithms.py
+++ HAGC_graph_algorithms/graph_algorithms.py
@@ -5,35 +5,41 @@ class Algorithms:
     
     def bfs(self, start_node):
         print("*** BFS ***")
+        # Check if the start node is in the graph
         if start_node not in self.graph:
             print("Node not found in graph")
             return -1
-        
+        # Create a list to store the visited nodes
         visited_nodes = []
+        # Create a queue to store the nodes to be visited
         queue = [start_node]
         
         while queue:
             node = queue.pop(0)
+            # Check if the node has been visited
             if node not in visited_nodes:
                 visited_nodes.append(node)
-
+                # Add the neighbors to the queue
                 for neighbor in self.graph[node]:
                     if neighbor not in visited_nodes:
                         queue.append(neighbor)
         return visited_nodes
 
     def dfs(self, start_node):
+        # Check if the start node is in the graph
         stack = [start_node]
+        # Create a list to store the visited nodes
         visited_node = set()
+        # Create a stack to store the nodes to be visited
         path = []
 
         while stack:
             current = stack.pop()
-            
+            # Check if the node has been visited
             if current not in visited_node:
                 visited_node.add(current)
                 path.append(current)
-
+                # Add the neighbors to the stack
                 for neighbor in self.graph[current]:
                     if neighbor not in visited_node:
                         stack.append(neighbor)
